9b0e7d091816c3a1c6d29a615fc980dca60ebe7b
Pablo Azagra <pazagra@unizar.es>
2018-04-10 10:55:38 +0200

    Update install.sh

--- MIL/install.sh
+++ MIL/install.sh
@@ -7,7 +7,7 @@ pip3 install scikit-image
 pip3 install cython
 pip3 install keras
 pip3 install h5py
-cd tfenv/maskrcnn/cocotools/coco/PythonAPI
-make install
 wget -nc --directory-prefix=./ https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5
 mv mask_rcnn_coco.h5 tfenv/maskrcnn/Mask_RCNN/mask_rcnn_coco.h5
+cd tfenv/maskrcnn/cocotools/coco/PythonAPI
+make install
